24ed5985554305a7eb86e36b0a10fc7c1c76edc1
Serdar Ayalp <serdarayalp@googlemail.com>
2020-07-20 09:50:52 +0200

    first commit

---
+++ start.py
@@ -0,0 +1,41 @@
+# Python ist eine Programmiersprache, die dank ihrer klaren Syntax und einfachen Lesbarkeit leicht zu 
+# erlernen ist und sich sehr vielseitig einsetzen lässt. Für die gängigen Betriebssysteme ist Python frei verfügbar. 
+# Python unterstützt mehrere Paradigmen der Programmierung wie die funktionale, objektorientierte oder aspektorientierte 
+# Programmierung und ist auch als Skriptsprache nutzbar. 
+
+# Python ist ein MUSS für Studenten und Berufstätige, besonders wenn sie im Bereich der Webentwicklung arbeiten.
+
+# Die wichtigsten Vorteile des Erlernens von Python:
+#   * Python wird interpretiert: Python wird zur Laufzeit vom Interpreter verarbeitet. Sie brauchen Ihr Programm 
+#       vor der Ausführung nicht zu kompilieren.
+#   * Python ist interaktiv: Sie können an einer Python-Eingabeaufforderung (Python prompt, IDLE) sitzen und direkt 
+#       mit dem Interpreter interagieren, um Ihre Programme zu schreiben.
+#   * Python ist objektorientiert: Python unterstützt einen objektorientierten Stil oder eine objektorientierte 
+#       Programmiertechnik, die Code innerhalb von Objekten kapselt.
+#   * Python ist eine Anfängersprache: Python ist eine großartige Sprache für Programmierer auf Anfängerniveau und 
+#       unterstützt die Entwicklung einer breiten Palette von Anwendungen, von einfacher Textverarbeitung über 
+#       WWW-Browser bis hin zu Spielen.
+
+
+# Wichtige Merkmale der Python-Programmierung:
+#   * Es unterstützt sowohl funktionale Programmiermethoden als auch OOP.
+#   * Es kann als Skriptsprache verwendet oder für die Erstellung großer Anwendungen in Bytecode kompiliert werden.
+#   * Sie stellt dynamische Datentypen auf sehr hohem Niveau zur Verfügung.
+#   * Es unterstützt die automatische Garbage Collection.
+#   * Leicht zu erlernen: Python hat wenige Schlüsselwörter, eine einfache Struktur und eine klar definierte Syntax. 
+#   * Leicht zu lesen: Python-Code ist klarer definiert und für das Auge sichtbar.
+#   * Leicht zu pflegen: Der Quellcode von Python ist ziemlich leicht zu pflegen.
+#   * Eine breite Standardbibliothek: Der Großteil der Python-Bibliothek ist sehr portabel und 
+#       plattformübergreifend kompatibel unter UNIX, Windows und Macintosh.
+#   * Interaktiver Modus: Python unterstützt einen interaktiven Modus, der interaktives Testen und Debuggen von 
+#       Code-Schnipseln ermöglicht.
+#   * Portabel: Python kann auf einer Vielzahl von Hardware-Plattformen laufen und hat auf allen Plattformen die 
+#       gleiche Schnittstelle.
+#   * Erweiterbar: Sie können dem Python-Interpreter Low-Level-Module hinzufügen. Diese Module ermöglichen es 
+#       Programmierern, ihre Werkzeuge zu ergänzen oder anzupassen, um effizienter zu arbeiten.
+#   * Datenbanken: Python bietet Schnittstellen zu allen wichtigen kommerziellen Datenbanken.
+#   * Skalierbar: Python bietet eine bessere Struktur und Unterstützung für große Programme als Shell-Skripting.
+
+
+print("Hello World...");
+print('Hello Python...');
