ef92b6d331c7e929ad3518b9665440cab50e8d48
Serdar Ayalp <serdarayalp@googlemail.com>
2020-08-25 13:21:41 +0200

    Strings

---
+++ strings.py
@@ -0,0 +1,41 @@
+str = "Hello World"
+
+# Slicing
+print("str[0]: ", str[0])
+print("str[1:5]: ", str[1:5])
+
+# Update Strings
+print(str[:6] + 'Python')
+
+a = "Hello"
+b = "Python"
+print(a + b)
+print(a * 2)
+print(a[1])
+print(a[1:4])  # ell
+print("H" in a)
+print("S" not in a)
+
+
+# String Formatting Operator
+print("My name is %s and weight is %d kg." % ('Max', 65))
+
+'''
+Format Symbol 	Conversion
+-------------------------------
+%c 	            character
+%s 	            String-Konvertierung mittels str() vor der Formatierung
+%i 	            signed decimal integer
+%d 	            signed decimal integer
+%u 	            unsigned decimal integer
+%o 	            octal integer
+%x 	            hexadecimal integer (lowercase letters)
+%X 	            hexadecimal integer (UPPERcase letters)
+%e 	            exponential notation (with lowercase 'e')
+%E 	            exponential notation (with UPPERcase 'E')
+%f 	            floating point real number
+%g 	            the shorter of %f and %e
+%G 	            the shorter of %f and %E
+'''
+
+# Triple Quotes
